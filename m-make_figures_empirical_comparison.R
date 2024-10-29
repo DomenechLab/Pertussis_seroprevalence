997945cdfc4d8de68fec3f5e1e36135dbc0631f5
Matthieu Domenech de Cellès <matthieu.domenech@gmail.com>
2024-10-29 10:28:10 +0100

    Add script to run homogeneous models

--- m-make_figures_empirical_comparison.R
+++ m-make_figures_empirical_comparison.R
@@ -4,64 +4,85 @@
 
 rm(list = ls())
 source("s-base_packages.R")
+library(lemon)
+library(latex2exp)
+library(ggrepel)
 debug_bool <- F
 theme_set(theme_bw())
 par(bty = "l", las = 1, lwd = 2)
+t_V <- 150 # Year of vaccine introduction
+path_to_res <- "_saved/estimation_6_countries/DR_40-rhoR_1.00/" # Path to saved results
+suffix <- str_extract(string = path_to_res, pattern = "DR_[0-9]+-rhoR_[0-9].[0-9]+") # String to append to PDF file name
 
-# Load seroprevalence estimates from Berbers ------------------------------
-library(lme4)
+# Load seroprevalence data ------------------------------------------------
+dat_seroprev <- read_xlsx(path = "_data/_seroprevalence/seroprevalence_data_Pebody_Wehlin.xlsx", 
+                          sheet = "data", 
+                          col_names = T) %>% 
+  left_join(y = data.frame(country = c("Finland", "France", "Germany", "Netherlands", "Lithuania", "Romania"), 
+                           country_short = c("FI", "FR", "DE", "NL", "LT", "RO"))) %>% 
+  select(country, country_short, everything()) %>% 
+  mutate(age_cat = factor(age_cat), 
+         age_cat = fct_recode(age_cat, "[65,Inf]" = "65+"), 
+         binom_test = map2(.x = n_pos, .y = n_tot, .f = \(x, y) binom.test(x = x, n = y, conf.level = 0.95))) %>% 
+  mutate(Sp_obs_est = map_dbl(.x = binom_test, .f = ~ pluck(.x, "estimate")), 
+         Sp_obs_inf = map_dbl(.x = binom_test, .f = ~ pluck(.x, "conf.int", 1)), 
+         Sp_obs_sup = map_dbl(.x = binom_test, .f = ~ pluck(.x, "conf.int", 2)))
 
-dat_sero <- read_xlsx(path = "_data/seroprevalence_data_Berbers_2021.xlsx")
-dat_sero <- dat_sero %>% 
-  mutate(country = factor(country), 
-         age = factor(age), 
-         n_neg = n_tot - n_pos, 
-         p_pos = n_pos / n_tot)
+# Load simulated data -----------------------------------------------------
 
-# Aggregate data over 40-49 and 50-59
-dat_sero_agg <- dat_sero %>% 
-  group_by(country) %>% 
-  summarise(n_pos = sum(n_pos), 
-            n_tot = sum(n_tot), 
-            n_neg = n_tot - n_pos,
-            p_pos = n_pos / n_tot) %>% 
-  ungroup()
-
-# Plot 
-pl <- ggplot(data = dat_sero_agg, mapping = aes(x = 1, y = 100 * p_pos)) + 
-  geom_boxplot(color = "grey") + 
-  geom_point(color = "black") + 
-  theme_classic() + 
-  labs(x = "", y = "Seroprevalence (%)")
-print(pl)
-
-# Fit mixed-effects binomial model
-# mod_bin <- glmer(cbind(n_pos, n_neg) ~ age + (1 | country), 
-#                  data = dat_sero, 
-#                  family = binomial(link = "logit"))
+# Load simulation details for every country
+sims_details <- read_xlsx(path = "_data/_seroprevalence/simulation_details.xlsx", sheet = "data", col_names = T)
 
 
-# Load simulations ------------------------------------------------------
-l_files <- list.files(path = "_saved", pattern = "DV", include.dirs = T)
+# List directories (each directory corresponds to a different rate of vaccine-derived immunity)
+l_dirs <- list.files(path = path_to_res, pattern = "DV", include.dirs = T)
+print(l_dirs)
 
-mod_preds <- vector(mode = "list", length = length(l_files))
+# Store results into a named list (name: average duration of vaccine-derived immunity)
+mod_preds <- vector(mode = "list", length = length(l_dirs))
+names(mod_preds) <- l_dirs %>% str_extract(pattern = "DV_[0-9]+") %>% str_remove(pattern = "DV_")
+
 for(i in seq_along(mod_preds)) {
-  mod_preds[[i]] <- readRDS(file = paste0("_saved/", l_files[i], "/all-", l_files[i], ".rds"))
+  
+  dir_cur <- l_dirs[i]
+  
+  # List files in current directory
+  l_files <- list.files(path = paste0(path_to_res, dir_cur), pattern = ".rds")
+  country_nm <- str_extract(string = l_files, pattern = "^([^-]+)") # Extract country names
+  
+  mod_preds[[i]] <- vector(mode = "list", length = length(l_files))
+  names(mod_preds[[i]]) <- country_nm
+  
+  # Load data
+  for(j in seq_along(l_files)) {
+    file_cur <- l_files[j]
+    country_cur <- country_nm[j]
+    mod_preds[[i]][[j]] <- readRDS(file = sprintf("%s%s/%s", path_to_res, dir_cur, file_cur)) %>% 
+      pluck("merged_ages") %>% 
+      filter(between(time, 
+                     sims_details$t1[sims_details$country == country_cur] + t_V, 
+                     sims_details$t2[sims_details$country == country_cur] + t_V), 
+             age_cat %in% dat_seroprev$age_cat)
+  }
+  
+  # Transform list into data frame
+  mod_preds[[i]] <- mod_preds[[i]] %>% 
+    bind_rows(.id = "country")
 }
 
-names(mod_preds) <- l_files %>% str_extract(pattern = "DV_[0-9]+") %>% str_remove(pattern = "DV_")
-
 mod_preds <- mod_preds %>% 
-  bind_rows(.id = "DV") %>% 
-  filter(age_cat %in% c("[40-50)", "[50,60)"), 
-         var_nm %in% c("Rp1", "Rp2", "Vp")) %>% 
-  group_by(DV, country, .id, time, var_nm) %>% 
+  bind_rows(.id = "DV")
+
+# Calculate seroprevalence and PPV ----------------------------------------
+mod_preds_sub <- mod_preds %>% 
+  filter(var_nm %in% c("Rp1", "Rp2", "Vp")) %>% 
+  group_by(DV, country, .id, time, age_cat, var_nm) %>% 
   summarise(n = sum(n), 
             pop = sum(pop)) %>% 
   ungroup() %>% 
   pivot_wider(names_from = "var_nm", values_from = "n") %>% 
-  mutate(sero_prev = (Rp1 + Rp2 + Vp) / pop, 
-         sero_PPV = Rp1 / (Rp1 + Rp2 + Vp), 
+  mutate(Sp_pred = (Rp1 + Rp2 + Vp) / pop, 
+         PPV_pred = Rp1 / (Rp1 + Rp2 + Vp), 
          DV = as.numeric(DV), 
          alphaV = 1 / DV)
 
@@ -72,31 +93,67 @@ f_list[[2]] <- function(x) quantile(x = x, probs = 0.5)
 f_list[[3]] <- function(x) quantile(x = x, probs = 0.975)
 names(f_list) <- c("q_inf", "q_med", "q_sup")
 
-mod_preds_sumry <- mod_preds %>% 
-  group_by(DV, alphaV) %>% 
-  summarise(across(c(sero_prev, sero_PPV), f_list)) %>% 
+mod_preds_sumry <- mod_preds_sub %>% 
+  filter(!is.na(PPV_pred)) %>% 
+  group_by(DV, alphaV, country, age_cat) %>% 
+  summarise(across(c(Sp_pred, PPV_pred), f_list)) %>% 
   ungroup()
 
+# Merge simulated and observed data
+pred_obs <- mod_preds_sumry %>% 
+  left_join(y = dat_seroprev %>% select(-c(binom_test))) %>% 
+  mutate(age_cat_no = ifelse(age_cat %in% c("[20,40)", "[20,45)"), 1, ifelse(age_cat == "[65,Inf]", 3, 2)), 
+         DV = as.factor(DV), 
+         DV_title = fct_relabel(.f = DV, .fun = ~ paste0("Dv = ", .x, " years"))) %>% 
+  select(DV, DV_title, alphaV, country, country_short, age_cat, age_cat_no, everything())
 
-# Plot --------------------------------------------------------------------
-pl <- ggplot(data = mod_preds_sumry, 
-             mapping = aes(x = 100 * sero_prev_q_med, y = 100 * sero_PPV_q_med, color = factor(100 * alphaV))) + 
-  geom_point() + 
-  geom_linerange(mapping = aes(xmin = 100 * sero_prev_q_inf, xmax = 100 * sero_prev_q_sup)) + 
-  geom_linerange(mapping = aes(ymin = 100 * sero_PPV_q_inf, ymax = 100 * sero_PPV_q_sup)) + 
-  geom_boxplot(data = dat_sero_agg, 
-               mapping = aes(x = 100 * p_pos, y = 0, color = NULL), 
-               width = 1, outlier.colour = "grey") + 
-  #scale_color_viridis(discrete = T, option = "mako", direction = -1, end = 0.8, begin = 0.2) + 
-  scale_color_brewer(palette = "Reds") + 
+# Estimation performance
+est_perf <- pred_obs %>% 
+  filter(!is.na(Sp_obs_est)) %>% 
+  group_by(DV, DV_title, alphaV, age_cat_no) %>% 
+  summarise(rmse = sqrt(mean((Sp_pred_q_med - Sp_obs_est) ^ 2)), # Root mean squared error
+            mab = mean(abs(Sp_pred_q_med - Sp_obs_est)) # Mean absolute boas
+  ) %>% 
+  ungroup()
+
+# Make plot in first adult age group--------------------------------------------------------------
+# See https://sahirbhatnagar.com/blog/2016/02/08/ggplot2-facet-wrap-labels/ for how to add the mathematical annotations
+appender <- function(string) TeX(paste0("$\\alpha_V = $", string, " per year"))
+
+pl <- ggplot(data = pred_obs %>% filter(age_cat_no == 1, country != "Ge"), 
+             mapping = aes(x = 100 * Sp_obs_est, 
+                           y = 100 * Sp_pred_q_med, 
+                           label = country_short)) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") + 
+  geom_linerange(mapping = aes(xmin = 100 * Sp_obs_inf, xmax = 100 * Sp_obs_sup), color = "grey") + 
+  geom_linerange(mapping = aes(ymin = 100 * Sp_pred_q_inf, ymax = 100 * Sp_pred_q_sup), color = "grey") + 
+  geom_point(mapping = aes(colour = 100 * PPV_pred_q_med, shape = cut_off), size = rel(3)) + 
+  scale_color_viridis(discrete = F, option = "magma", direction = 1, begin = 0, end = 1, trans = "log") + 
+  scale_shape_manual(values = c(17, 16)) + 
+  geom_text_repel(force_pull = 0) + 
+  geom_text(data = est_perf %>% mutate(x = 6, y = 0) %>% filter(age_cat_no == 1), 
+            mapping = aes(x = x, y = y, label = paste0("MAB = ", round(100 * mab, 1), "%")), 
+            size = 11, size.unit = "pt") + 
+  facet_rep_wrap(~ as.character(round(alphaV, 2)), 
+             labeller = as_labeller(x = appender, default = label_parsed), 
+             scales = "fixed", 
+             dir = "h", 
+             ncol = 2) + 
   theme_classic() + 
-  theme(legend.position = "top") + 
-  labs(x = "Seroprevalence (%)", y = "PPV of seropositivity (%)", 
-       color = "Waning rate of vaccine-derived immunity (% per yr)")
+  theme(legend.position = "top", 
+        panel.grid = element_blank(),
+        #axis.line = element_line(color = "black", size = 0.5),  # Keep axis 
+        strip.background = element_blank(), 
+        strip.text = element_text(size = 11)) + 
+  labs(x = "Observed seroprevalence (%)", 
+       y = "Predicted seroprevalence (%)", 
+       color = "PPV (%)", 
+       shape = "IgG seropositivity threshold")
 print(pl)
 
-ggsave(filename = "_figures/test_main.pdf", plot = pl, width = 7, height = 7)
-  
+ggsave(filename = sprintf("_figures/main/fig_empirical_comparison-%s.pdf", suffix), 
+       plot = pl, width = 8, height = 8)
+
 #######################################################################################################
 # END 
 #######################################################################################################  
---
+++ s-simulate_SIRWS_model.R
@@ -0,0 +1,262 @@
+#######################################################################################################
+# Run simulations of homogeneous SIRWS model with vaccination
+# Time unit: years; rates are per year
+#######################################################################################################
+
+rm(list = ls())
+source("s-base_packages.R")
+library(pomp)
+library(metR)
+theme_set(theme_bw())
+par(bty = "l", las = 1, lwd = 2)
+
+# Csnippet for ODE models --------------------------------------------------
+
+names_mod <- c("SIRWS", "SIRWS_bis", "SIRRp")
+skel <- vector(mode = "list", length = 3)
+names(skel) <- names_mod
+
+skel[["SIRWS"]] <- Csnippet("
+  double b = R0 * (gamma + mu); 
+  double lambda = b * I; 
+  
+  DV = mu * p_V + rho_V * lambda * W_V - (2.0 * alpha_V + mu) * V; 
+  DW_V = 2.0 * alpha_V * V - (rho_V * lambda + 2.0 * alpha_V + mu) * W_V; 
+  DS = mu * (1.0 - p_V) + 2.0 * alpha_V * W_V + 2.0 * alpha_R * W_R - (lambda + mu) * S; 
+  DI = lambda * S - (gamma + mu) * I; 
+  DR = gamma * I + rho_R * lambda * W_R - (2.0 * alpha_R + mu) * R; 
+  DW_R = 2.0 * alpha_R * R - (2.0 * alpha_R + rho_R * lambda + mu) * W_R; 
+")
+
+skel[["SIRWS_bis"]] <- Csnippet("
+  double b = R0 * (gamma + mu); // Transmission rate
+  double lambda = b * I; // Force of infection
+  double alpha_prime = 1.0 / (0.5 / alpha_R - 1.0 / alpha_P); // Rate of transition R->W_R
+  
+  DV = mu * p_V + alpha_P * Vp - (rho_V * lambda + alpha_V + mu) * V; 
+  DVp = rho_V * lambda * V - (alpha_P + mu) * Vp; 
+  DS = mu * (1.0 - p_V) + alpha_V * V + 2.0 * alpha_R * W_R - (lambda + mu) * S; 
+  DI = lambda * S - (gamma + mu) * I; 
+  DRp = gamma * I + rho_R * lambda * W_R - (alpha_P + mu) * Rp;
+  DR = alpha_P * Rp - (alpha_prime + mu) * R; 
+  DW_R = alpha_prime * R - (2.0 * alpha_R + rho_R * lambda + mu) * W_R; 
+")
+
+skel[["SIRRp"]] <- Csnippet("
+  double b = R0 * (gamma + mu); 
+  double lambda = b * I; 
+  
+  DV = mu * p_V + alpha_P * Vp - (rho_V * lambda + alpha_V + mu) * V; 
+  DVp = rho_V * lambda * V - (alpha_P + mu) * Vp; 
+  DS = mu * (1.0 - p_V) + alpha_V * V + alpha_R * R - (lambda + mu) * S; 
+  DI = lambda * S - (gamma + mu) * I; 
+  DRp1 = gamma * I - (alpha_P + mu) * Rp1;
+  DR = alpha_P * Rp1 + alpha_P * Rp2 - (rho_R * lambda + alpha_R + mu) * R; 
+  DRp2 = rho_R * lambda * R - (alpha_P + mu) * Rp2; 
+")
+
+# Parameters -------------------------------------------------------
+theta <- vector(mode = "list", length = 3)
+names(theta) <- names_mod
+
+theta[["SIRWS"]] <-  c(
+  "mu" = 1 / 75, # Birth/death rate 
+  "R0" = 16, # Basic reproduction number
+  "gamma" = 1 / (3.7 / 52), # Recovery rate
+  "p_V" = 0, # Effective vaccine coverage
+  "alpha_V" = 1 / 34, # Rate of waning vaccine immunity (without boosting)
+  "rho_V" = 0.25, # Boosting coefficient of vaccine immunity
+  "alpha_R" = 1 / 34, # Rate of waning infection-derived immunity (without boosting)
+  "rho_R" = 6.6 # Boosting coefficient of infection-derived immunity
+)
+
+theta[["SIRRp"]] <- theta[["SIRWS_bis"]] <- theta[["SIRWS"]]
+theta[["SIRRp"]] <- c(theta[["SIRRp"]], "alpha_P" = 1 / 1)
+theta[["SIRWS_bis"]] <- c(theta[["SIRWS_bis"]], "alpha_P" = 1 / 1)
+
+# Names of state variables
+state_nm <- vector(mode = "list", length = 3)
+names(state_nm) <- names_mod
+
+state_nm[["SIRWS"]] <-  c("V", "W_V", "S", "I", "R", "W_R")
+state_nm[["SIRWS_bis"]] <- c("V", "Vp", "S", "I", "Rp", "R", "W_R")
+state_nm[["SIRRp"]] <- c("V", "Vp", "S", "I", "Rp1", "R", "Rp2")
+
+for(i in seq_along(theta)) {
+  theta[[i]] <- c(theta[[i]], setNames(object = rep(0, length(state_nm[[i]])), 
+                                       nm = paste0(state_nm[[i]], ".0")))
+  theta[[i]]["V.0"] <- unname(theta[["SIRWS"]]["p_V"])
+  theta[[i]]["S.0"] <- unname(1 - theta[["SIRWS"]]["p_V"] - 1e-3)
+  theta[[i]]["I.0"] <- 1e-3
+}
+
+# Transmission rate
+beta_val <- unname(theta[[1]]["R0"] * (theta[[1]]["gamma"] + theta[[1]]["mu"]))
+
+# Create POMP models ------------------------------------------------------
+mod <- vector(mode = "list", length = 3)
+names(mod) <- names_mod
+
+for(i in seq_along(mod)) {
+  
+  mod[[i]] <- pomp(data = data.frame(time = seq(0, 500, by = 0.05), X = NA),
+                   times = "time",
+                   t0 = 0,
+                   obsnames = "X",
+                   statenames = state_nm[[i]],
+                   paramnames = names(theta[[i]]), 
+                   params = theta[[i]],
+                   skeleton = vectorfield(f = skel[[i]]))
+  stopifnot(sum(rinit(mod[[i]])) == 1)
+  print(i)
+}
+
+# Test simulation ----------------------------------------------------------------
+for(i in seq_along(mod)) {
+  
+  tj <- trajectory(object = mod[[i]], format = "data.frame") 
+  tj$N <- rowSums(tj[, state_nm[[i]]])
+  
+  tj <- tj %>% 
+    mutate(lambda = beta_val * I)
+  
+  tj_long <- tj %>% 
+    pivot_longer(cols = -c(".id", "time"), names_to = "var", values_to = "prop")
+  
+  pl <- ggplot(data = tj_long, mapping = aes(x = time, y = prop)) + 
+    geom_line() + 
+    facet_wrap(~ var, scales = "free_y") +
+    scale_y_sqrt() + 
+    labs(x = "Time (years)", y = "Proportion", title = names(mod)[i])
+  print(pl)
+  
+  print(filter(tj_long, time == max(time)))
+}
+
+# Simulate SIRRp model ----------------------------------------------------
+p_V <- 0.9
+which_mod <- "SIRRp"
+coef(mod[[which_mod]], names(theta[[which_mod]])) <- unname(theta[[which_mod]])
+coef(mod[[which_mod]], "p_V") <- p_V
+coef(mod[[which_mod]], "alpha_P") <- 1 / 0.75
+coef(mod[[which_mod]], c("V.0", "S.0", "I.0")) <- c(p_V, 1 - p_V - 1e-3, 1e-3)
+coef(mod[[which_mod]], c("rho_R", "alpha_R")) <- c(1.9, 1 / 360.2)
+coef(mod[[which_mod]], c("rho_V", "alpha_V")) <- c(1, 0.02)
+
+stopifnot(sum(rinit(mod[[which_mod]])) == 1)
+
+tj <- trajectory(object = mod[[which_mod]], format = "data.frame") 
+tj$N <- rowSums(tj[, state_nm[[which_mod]]])
+
+tj <- tj %>% 
+  mutate(lambda = beta_val * I, 
+         Sp = Vp + Rp1 + Rp2)
+
+tj_long <- tj %>% 
+  pivot_longer(cols = -c(".id", "time"), names_to = "var", values_to = "prop")
+
+pl <- ggplot(data = tj_long, mapping = aes(x = time, y = prop)) + 
+  geom_line() + 
+  facet_wrap(~ var, scales = "free_y") +
+  scale_y_sqrt() + 
+  labs(x = "Time (years)", y = "Proportion", title = names(mod[[i]]))
+print(pl)
+
+print(filter(tj_long, time == max(time)))
+
+# Grid search to calibrate rho_R -------------------------------------------------------------
+all_parms <- expand_grid(rho_R = seq(5, 20, length.out = 20), 
+                         alpha_R = 1 / seq(10, 100, length.out = 20)) %>% 
+  mutate(.id = seq_len(nrow(.)))
+
+p_mat <- parmat(params = coef(mod[["SIRRp"]]), 
+                nrep = nrow(all_parms))
+p_mat["rho_R", ] <- all_parms$rho_R
+p_mat["alpha_R", ] <- all_parms$alpha_R
+
+tj_multi <- bake(file = "_saved/other/simulations_SIRRp_model.rds", 
+                 expr = {
+                   trajectory(object = mod[["SIRRp"]], 
+                              params = p_mat,
+                              format = "data.frame") %>% 
+                     mutate(.id = as.integer(.id), 
+                            Sp = Rp1 + Rp2,
+                            lambda = beta_val * I) %>% 
+                     left_join(y = all_parms)
+                 })
+  
+
+tj_multi_long <- tj_multi %>% 
+  pivot_longer(cols = -c(".id", "time", "rho_R", "alpha_R"), names_to = "var", values_to = "prop")
+
+eq_vals <- tj_multi %>% filter(time == max(time))
+
+pl <- ggplot(data = eq_vals, mapping = aes(x = rho_R, y = 1 / alpha_R, z = lambda - 0.231)) + 
+  geom_tile(mapping = aes(fill = lambda - 0.231)) + 
+  geom_contour(color = "black") + 
+  geom_text_contour(color = "black") +
+  scale_fill_gradient2(midpoint = 0, low = "blue", mid = "white", high = "red") + 
+  #scale_fill_scico(palette = "vik", midpoint = 0) + 
+  labs(x = expression(rho[R]), y = expression(D[R]))
+print(pl)
+
+pl <- ggplot(data = eq_vals, mapping = aes(x = rho_R, y = 1 / alpha_R, z = 100 * Sp)) + 
+  geom_tile(mapping = aes(fill = 100 * Sp)) + 
+  geom_contour(color = "white") + 
+  geom_text_contour(color = "white") +
+  scale_fill_viridis(option = "viridis") + 
+  labs(x = expression(rho[R]), y = expression(D[R]))
+print(pl)
+
+# Estimate rho_R and alpha_R to reach target FoI and seroprevalence ----------------------------------------------
+coef(mod[["SIRRp"]], names(theta[["SIRRp"]])) <- unname(theta[["SIRRp"]])
+
+dist_fun <- function(par, FoI_tar, Sp_tar, cut_off = 125) {
+  # Args: 
+  # par: parameters to estimate (rho_R, alpha_R) [2-vector]
+  # FoI_tar: target force of infection (per year) [scalar]
+  # Sp_tar: target seroprevalence [scalar]
+  # Returns: squared distance between simulated and target FoI and Sp
+  
+  # Set parameters
+  rho_R_test <- exp(par[1])
+  D_R_test <- exp(par[2])
+  
+  if(cut_off == 125) {
+    coef(mod[["SIRRp"]], "alpha_P") <- 1 / 0.75
+  } else if(cut_off == 50) {
+    coef(mod[["SIRRp"]], "alpha_P") <- 1 / 2.42
+  }
+  
+  coef(mod[["SIRRp"]], c("alpha_R", "rho_R")) <- c(1 / D_R_test, rho_R_test)
+  
+  # Run model until equilibrium
+  sim_cur <- trajectory(object = mod[["SIRRp"]], format = "data.frame") %>% 
+    mutate(Sp = Rp1 + Rp2, 
+           FoI = beta_val * I)
+  
+  # Extract equilibrium
+  Sp_pred <- sim_cur$Sp[which.max(sim_cur$time)]
+  FoI_pred <- sim_cur$FoI[which.max(sim_cur$time)]
+  
+  out <- ((Sp_pred - Sp_tar) ^ 2) + ((FoI_pred - FoI_tar) ^ 2)
+  return(out)
+}
+
+x <- dist_fun(par = log(c(5, 10)), FoI_tar = 0.231, Sp_tar = 0.5, cut_off = 125)
+
+fit <- optim(par = log(c(5, 10)), 
+             fn = dist_fun, 
+             FoI_tar = 0.257, 
+             Sp_tar = 0.238, 
+             cut_off = 125,
+             method = "Nelder-Mead")
+stopifnot(fit$convergence == 0)
+
+par_est <- exp(fit$par)
+print(par_est %>% round(1))
+print(fit$value)
+
+#######################################################################################################
+# END
+#######################################################################################################
